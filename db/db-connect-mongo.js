c82ed38011a619a063f94cc50045ccb77d7ec025
Anderson Londoño <84043589+Anderlonva@users.noreply.github.com>
2023-01-10 13:43:29 -0500

    gitignore .env

--- db/db-connect-mongo.js
+++ db/db-connect-mongo.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const getConnection = async () => {
     try {
-        const url = 'mongodb://admin:admin@ac-jyqsqvn-shard-00-00.ecuhkoo.mongodb.net:27017,ac-jyqsqvn-shard-00-01.ecuhkoo.mongodb.net:27017,ac-jyqsqvn-shard-00-02.ecuhkoo.mongodb.net:27017/inventarios?ssl=true&replicaSet=atlas-j5j5jx-shard-0&authSource=admin&retryWrites=true&w=majority';
+        const url = 'mongodb://'+process.env.USER +':'+ process.env.PASSWORD+'@ac-jyqsqvn-shard-00-00.ecuhkoo.mongodb.net:27017,ac-jyqsqvn-shard-00-01.ecuhkoo.mongodb.net:27017,ac-jyqsqvn-shard-00-02.ecuhkoo.mongodb.net:27017/inventarios?ssl=true&replicaSet=atlas-j5j5jx-shard-0&authSource=admin&retryWrites=true&w=majority';
 
         await mongoose.connect(url);
 
